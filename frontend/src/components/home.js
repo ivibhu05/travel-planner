9daabf2d661cd8e278742f0981f3fb21b69ac2cf
Hemant Kumar Singh <shemant820@gmail.com>
2024-08-28 23:13:50 +0530

    added route

---
+++ frontend/src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+
+        <h2>
+         Travel Planner App!
+            </h2>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
