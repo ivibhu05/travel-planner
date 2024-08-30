5dd9de9d406af8177f361b127bb74adabfc2e51c
Hemant Kumar Singh <shemant820@gmail.com>
2024-08-31 00:59:54 +0530

    added initial frontend components , routes

---
+++ frontend/src/static/homeCss.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const flexBoxContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-image: url('../static/images/nature-bg.jpg');
+    background-size: cover;
+    background-position: center;
+`;
+
